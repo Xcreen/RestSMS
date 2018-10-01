1ed7ad67095c254d31f9c1334c38bbad4ca8b3ab
Xcreen <Xcreen@users.noreply.github.com>
2018-10-01 14:42:03 +0200

    Ask for SMS-Permission

--- app/src/main/java/net/xcreen/restsms/HomeFragment.java
+++ app/src/main/java/net/xcreen/restsms/HomeFragment.java
@@ -1,8 +1,11 @@
 package net.xcreen.restsms;
 
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.telephony.TelephonyManager;
@@ -16,6 +19,7 @@ public class HomeFragment extends Fragment {
 
     Button toggleServerBtn;
     AppContext appContext;
+    int SMS_PERMISSION_REQUEST = 100;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -32,66 +36,70 @@ public class HomeFragment extends Fragment {
         toggleServerBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //Check if Device has a Sim-Card
-                try {
-                    TelephonyManager telephonyManager = (TelephonyManager) getContext().getSystemService(Context.TELEPHONY_SERVICE);
-                    int primarySim = telephonyManager.getSimState();
-                    int secondarySim = TelephonyManager.SIM_STATE_ABSENT;
-                    if (Build.VERSION.SDK_INT >= 26) {
-                        primarySim = telephonyManager.getSimState(0);
-                        secondarySim = telephonyManager.getSimState(1);
-                    }
-                    if (primarySim != TelephonyManager.SIM_STATE_READY) {
-                        if (secondarySim != TelephonyManager.SIM_STATE_READY) {
-                            //Device has not Sim-Card which is ready
-                            Toast.makeText(getContext(), getResources().getText(R.string.invalid_sim), Toast.LENGTH_SHORT).show();
-                            return;
+                //Check if SMS-Permission is grant
+                if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.SEND_SMS) == PackageManager.PERMISSION_GRANTED) {
+                    //Check if Device has a Sim-Card
+                    try {
+                        TelephonyManager telephonyManager = (TelephonyManager) getContext().getSystemService(Context.TELEPHONY_SERVICE);
+                        int primarySim = telephonyManager.getSimState();
+                        int secondarySim = TelephonyManager.SIM_STATE_ABSENT;
+                        if (Build.VERSION.SDK_INT >= 26) {
+                            primarySim = telephonyManager.getSimState(0);
+                            secondarySim = telephonyManager.getSimState(1);
                         }
+                        if (primarySim != TelephonyManager.SIM_STATE_READY) {
+                            if (secondarySim != TelephonyManager.SIM_STATE_READY) {
+                                //Device has not Sim-Card which is ready
+                                Toast.makeText(getContext(), getResources().getText(R.string.invalid_sim), Toast.LENGTH_SHORT).show();
+                                return;
+                            }
+                        }
+                    } catch (Exception ex) {
+                        ex.printStackTrace();
+                        Toast.makeText(getContext(), getResources().getText(R.string.invalid_sim), Toast.LENGTH_SHORT).show();
+                        return;
                     }
-                }
-                catch (Exception ex){
-                    ex.printStackTrace();
-                    Toast.makeText(getContext(), getResources().getText(R.string.invalid_sim), Toast.LENGTH_SHORT).show();
-                    return;
-                }
 
-                //Check if Server is running
-                if(appContext.smsServer.isRunning() && !appContext.smsServer.isStopping()){
-                    //Stop Server
-                    new Thread(new Runnable() {
-                        @Override
-                        public void run() {
-                            try {
-                                appContext.smsServer.stop();
+                    //Check if Server is running
+                    if (appContext.smsServer.isRunning() && !appContext.smsServer.isStopping()) {
+                        //Stop Server
+                        new Thread(new Runnable() {
+                            @Override
+                            public void run() {
+                                try {
+                                    appContext.smsServer.stop();
+                                } catch (Exception ex) {
+                                    ex.printStackTrace();
+                                }
                             }
-                            catch (Exception ex){
-                                ex.printStackTrace();
+                        }).start();
+                        //Switch Button-Text
+                        toggleServerBtn.setText(getResources().getText(R.string.start_server));
+                    } else if (!appContext.smsServer.isStopping()) {
+                        //Start Server
+                        new Thread(new Runnable() {
+                            @Override
+                            public void run() {
+                                try {
+                                    appContext.smsServer.start(getContext().getCacheDir().getAbsolutePath());
+                                } catch (Exception ex) {
+                                    ex.printStackTrace();
+                                    Toast.makeText(getContext(), getResources().getText(R.string.server_failed_to_start), Toast.LENGTH_SHORT).show();
+                                }
                             }
-                        }
-                    }).start();
-                    //Switch Button-Text
-                    toggleServerBtn.setText(getResources().getText(R.string.start_server));
-                }
-                else if(!appContext.smsServer.isStopping()){
-                    //Start Server
-                    new Thread(new Runnable() {
-                        @Override
-                        public void run() {
-                            try {
-                                appContext.smsServer.start(getContext().getCacheDir().getAbsolutePath());
-                            }
-                            catch (Exception ex){
-                                ex.printStackTrace();
-                                Toast.makeText(getContext(), getResources().getText(R.string.server_failed_to_start), Toast.LENGTH_SHORT).show();
-                            }
-                        }
-                    }).start();
-                    //Switch Button-Text
-                    toggleServerBtn.setText(getResources().getText(R.string.stop_server));
+                        }).start();
+                        //Switch Button-Text
+                        toggleServerBtn.setText(getResources().getText(R.string.stop_server));
+                    } else {
+                        //Server is stopping
+                        Toast.makeText(getContext(), getResources().getText(R.string.server_is_stopping), Toast.LENGTH_SHORT).show();
+                    }
                 }
                 else{
-                    //Server is stopping
-                    Toast.makeText(getContext(), getResources().getText(R.string.server_is_stopping), Toast.LENGTH_SHORT).show();
+                    //Show Error Toast
+                    Toast.makeText(getContext(), getResources().getText(R.string.no_sms_permission), Toast.LENGTH_LONG).show();
+                    //Request Permission
+                    ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.SEND_SMS}, SMS_PERMISSION_REQUEST);
                 }
             }
         });
