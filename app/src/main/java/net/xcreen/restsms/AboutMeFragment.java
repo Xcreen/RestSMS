1b9fac996659f2ca373c90e9406564708eb0a2a1
Xcreen <Xcreen@users.noreply.github.com>
2018-10-05 11:05:58 +0200

    Added About-Me Page

--- app/src/main/java/net/xcreen/restsms/AboutMeFragment.java
+++ app/src/main/java/net/xcreen/restsms/AboutMeFragment.java
@@ -1,12 +1,15 @@
 package net.xcreen.restsms;
 
+import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.browser.customtabs.CustomTabsIntent;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 public class AboutMeFragment extends Fragment {
@@ -17,9 +20,35 @@ public class AboutMeFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_about_me, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_about_me, container, false);
+
+        Button donateBtn = rootView.findViewById(R.id.about_me_donate_btn);
+        donateBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                CustomTabsIntent.Builder builder = new CustomTabsIntent.Builder();
+                builder.setToolbarColor(getResources().getColor(R.color.colorDarkBlack, null));
+                CustomTabsIntent customTabsIntent = builder.build();
+
+                //Open Paypal Website
+                customTabsIntent.launchUrl(getContext(), Uri.parse("https://www.paypal.me/xcreen"));
+            }
+        });
+
+        Button githubBtn = rootView.findViewById(R.id.about_me_github_btn);
+        githubBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                CustomTabsIntent.Builder builder = new CustomTabsIntent.Builder();
+                builder.setToolbarColor(getResources().getColor(R.color.colorDarkBlack, null));
+                CustomTabsIntent customTabsIntent = builder.build();
+
+                //Open Github Website
+                customTabsIntent.launchUrl(getContext(), Uri.parse("https://github.com/Xcreen"));
+            }
+        });
+
+        return rootView;
     }
 }
