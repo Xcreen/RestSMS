702593dbfda34ea5b28fc70721c8f3ad842f137c
Xcreen <Xcreen@users.noreply.github.com>
2018-10-08 14:43:50 +0200

    Use correct context

--- app/src/main/java/net/xcreen/restsms/HomeFragment.java
+++ app/src/main/java/net/xcreen/restsms/HomeFragment.java
@@ -37,10 +37,10 @@ public class HomeFragment extends Fragment {
             @Override
             public void onClick(View v) {
                 //Check if SMS-Permission is grant
-                if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.SEND_SMS) == PackageManager.PERMISSION_GRANTED) {
+                if (ActivityCompat.checkSelfPermission(v.getContext(), Manifest.permission.SEND_SMS) == PackageManager.PERMISSION_GRANTED) {
                     //Check if Device has a Sim-Card
                     try {
-                        TelephonyManager telephonyManager = (TelephonyManager) getContext().getSystemService(Context.TELEPHONY_SERVICE);
+                        TelephonyManager telephonyManager = (TelephonyManager) v.getContext().getSystemService(Context.TELEPHONY_SERVICE);
                         int primarySim = telephonyManager.getSimState();
                         int secondarySim = TelephonyManager.SIM_STATE_ABSENT;
                         if (Build.VERSION.SDK_INT >= 26) {
@@ -50,13 +50,13 @@ public class HomeFragment extends Fragment {
                         if (primarySim != TelephonyManager.SIM_STATE_READY) {
                             if (secondarySim != TelephonyManager.SIM_STATE_READY) {
                                 //Device has not Sim-Card which is ready
-                                Toast.makeText(getContext(), getResources().getText(R.string.invalid_sim), Toast.LENGTH_SHORT).show();
+                                Toast.makeText(v.getContext(), getResources().getText(R.string.invalid_sim), Toast.LENGTH_SHORT).show();
                                 return;
                             }
                         }
                     } catch (Exception ex) {
                         ex.printStackTrace();
-                        Toast.makeText(getContext(), getResources().getText(R.string.invalid_sim), Toast.LENGTH_SHORT).show();
+                        Toast.makeText(v.getContext(), getResources().getText(R.string.invalid_sim), Toast.LENGTH_SHORT).show();
                         return;
                     }
 
@@ -76,12 +76,13 @@ public class HomeFragment extends Fragment {
                         //Switch Button-Text
                         toggleServerBtn.setText(getResources().getText(R.string.start_server));
                     } else if (!appContext.smsServer.isStopping()) {
+                        final String cacheDir = v.getContext().getCacheDir().getAbsolutePath();
                         //Start Server
                         new Thread(new Runnable() {
                             @Override
                             public void run() {
                                 try {
-                                    appContext.smsServer.start(getContext().getCacheDir().getAbsolutePath());
+                                    appContext.smsServer.start(cacheDir);
                                 } catch (Exception ex) {
                                     ex.printStackTrace();
                                     Toast.makeText(getContext(), getResources().getText(R.string.server_failed_to_start), Toast.LENGTH_SHORT).show();
@@ -92,12 +93,12 @@ public class HomeFragment extends Fragment {
                         toggleServerBtn.setText(getResources().getText(R.string.stop_server));
                     } else {
                         //Server is stopping
-                        Toast.makeText(getContext(), getResources().getText(R.string.server_is_stopping), Toast.LENGTH_SHORT).show();
+                        Toast.makeText(v.getContext(), getResources().getText(R.string.server_is_stopping), Toast.LENGTH_SHORT).show();
                     }
                 }
                 else{
                     //Show Error Toast
-                    Toast.makeText(getContext(), getResources().getText(R.string.no_sms_permission), Toast.LENGTH_LONG).show();
+                    Toast.makeText(v.getContext(), getResources().getText(R.string.no_sms_permission), Toast.LENGTH_LONG).show();
                     //Request Permission
                     ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.SEND_SMS}, SMS_PERMISSION_REQUEST);
                 }
