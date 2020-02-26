7b3712a87b867e2b35c41a1a6e99b6929dc42c6d
Xcreen <Xcreen@users.noreply.github.com>
2020-02-26 10:39:00 +0100

    Rewrite: LoggingDetailFragment

---
+++ app/src/main/java/net/xcreen/restsms/fragments/LoggingDetailFragment.kt
@@ -0,0 +1,58 @@
+package net.xcreen.restsms.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import net.xcreen.restsms.R
+import java.io.BufferedReader
+import java.io.File
+import java.io.FileReader
+
+class LoggingDetailFragment : Fragment() {
+    private var logFilePath: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        try {
+            logFilePath = arguments!!.getString("logfilepath")
+        }
+        catch (ex: Exception) {
+            ex.printStackTrace()
+        }
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val rootView = inflater.inflate(R.layout.fragment_logging_detail, container, false)
+        val logFile = File(logFilePath!!)
+        val headlineTextview = rootView.findViewById<TextView>(R.id.logging_detail_headline_textview)
+        headlineTextview.text = resources.getString(R.string.logging_detail_headline, logFile.name)
+        val loggingTextview = rootView.findViewById<TextView>(R.id.logging_detail_textview)
+        try {
+            val bufferedReader = BufferedReader(FileReader(logFile))
+            var line = ""
+            var logFileText = ""
+            while (bufferedReader.readLine().also { line = it } != null) {
+                logFileText += line + "\n"
+            }
+            loggingTextview.text = logFileText
+        }
+        catch (ex: Exception) {
+            ex.printStackTrace()
+        }
+        return rootView
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance(logFilePath: String?): LoggingDetailFragment {
+            val fragment = LoggingDetailFragment()
+            val bundle = Bundle(1)
+            bundle.putString("logfilepath", logFilePath)
+            fragment.arguments = bundle
+            return fragment
+        }
+    }
+}
