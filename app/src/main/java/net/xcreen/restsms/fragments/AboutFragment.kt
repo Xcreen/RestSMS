83f7af947ff290c06bc4f92f4a09c1d6711cbd54
Xcreen <Xcreen@users.noreply.github.com>
2022-02-23 23:07:34 +0100

    Change About-Fragment Viewpage to Viewpager2 (Deprecation)

--- app/src/main/java/net/xcreen/restsms/fragments/AboutFragment.kt
+++ app/src/main/java/net/xcreen/restsms/fragments/AboutFragment.kt
@@ -6,27 +6,26 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.widget.LinearLayoutCompat
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentManager
-import androidx.fragment.app.FragmentStatePagerAdapter
-import androidx.viewpager.widget.ViewPager
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import androidx.viewpager2.widget.ViewPager2
 import com.google.android.material.appbar.AppBarLayout
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayout.OnTabSelectedListener
-import com.google.android.material.tabs.TabLayout.TabLayoutOnPageChangeListener
+import com.google.android.material.tabs.TabLayoutMediator
 import net.xcreen.restsms.R
 
 class AboutFragment : Fragment() {
     private var tabLayout: TabLayout? = null
-    private var viewPager: ViewPager? = null
+    private var viewPager: ViewPager2? = null
     private var mainAppBarLayout: AppBarLayout? = null
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val rootView = inflater.inflate(R.layout.fragment_about, container, false)
         if (activity != null) { //Set Tabs
             tabLayout = TabLayout(requireActivity())
-            tabLayout!!.addTab(tabLayout!!.newTab().setText(resources.getText(R.string.about_about_me)))
-            tabLayout!!.addTab(tabLayout!!.newTab().setText(resources.getText(R.string.about_app_information)))
-            tabLayout!!.addTab(tabLayout!!.newTab().setText(resources.getText(R.string.about_third_party_librarys)))
+            tabLayout!!.addTab(tabLayout!!.newTab()) // About-Me Tab
+            tabLayout!!.addTab(tabLayout!!.newTab()) // App-Info Tab
+            tabLayout!!.addTab(tabLayout!!.newTab()) // Third-Party-Info Tab
             tabLayout!!.tabGravity = TabLayout.GRAVITY_FILL
             tabLayout!!.setBackgroundColor(resources.getColor(R.color.colorDarkBlack, null))
             tabLayout!!.setTabTextColors(resources.getColor(R.color.colorWhite, null), resources.getColor(R.color.colorLightBlue, null))
@@ -41,10 +40,17 @@ class AboutFragment : Fragment() {
             mainAppBarLayout = requireActivity().findViewById(R.id.appbar_layout)
             mainAppBarLayout!!.addView(tabLayout, LinearLayoutCompat.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT))
             //Set Page-Adapter to ViewPager
-            val pagerAdapter = FragmentPageAdapter(parentFragmentManager, tabLayout!!.tabCount)
+            val pagerAdapter = FragmentPageAdapter(this, tabLayout!!.tabCount)
             viewPager = rootView.findViewById(R.id.about_view_pager)
             viewPager!!.adapter = pagerAdapter
-            viewPager!!.addOnPageChangeListener(TabLayoutOnPageChangeListener(tabLayout))
+            TabLayoutMediator(tabLayout!!, viewPager!!) { tab, position ->
+                tab.text = when (position) {
+                    0 -> resources.getText(R.string.about_about_me)
+                    1 -> resources.getText(R.string.about_app_information)
+                    2 -> resources.getText(R.string.about_third_party_librarys)
+                    else -> resources.getText(R.string.about_about_me)
+                }
+            }.attach()
         }
         return rootView
     }
@@ -61,18 +67,17 @@ class AboutFragment : Fragment() {
     }
 }
 
-internal class FragmentPageAdapter(fm: FragmentManager?, private val numberOfTabs: Int) : FragmentStatePagerAdapter(fm!!, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
-
-    override fun getItem(position: Int): Fragment {
+internal class FragmentPageAdapter(fragment: Fragment, private val numberOfTabs: Int) : FragmentStateAdapter(fragment) {
+    override fun createFragment(position: Int): Fragment {
         return when (position) {
+            0 -> AboutMeFragment()
             1 -> AboutAppFragment()
             2 -> AboutThirdPartyLibrarysFragment()
-            0 -> AboutMeFragment()
             else -> AboutMeFragment()
         }
     }
 
-    override fun getCount(): Int {
+    override fun getItemCount(): Int {
         return numberOfTabs
     }
 }
