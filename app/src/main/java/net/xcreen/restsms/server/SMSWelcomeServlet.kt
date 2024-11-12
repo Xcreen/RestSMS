07428e9008c1edac04ca2c84f79be144d71f20f5
Xcreen <Xcreen@users.noreply.github.com>
2024-11-12 22:41:30 +0100

    Fix possible null

--- app/src/main/java/net/xcreen/restsms/server/SMSWelcomeServlet.kt
+++ app/src/main/java/net/xcreen/restsms/server/SMSWelcomeServlet.kt
@@ -1,7 +1,6 @@
 package net.xcreen.restsms.server
 
 import java.io.IOException
-import java.net.InetAddress
 import java.net.NetworkInterface
 import java.util.*
 import javax.servlet.http.HttpServlet
@@ -25,12 +24,12 @@ class SMSWelcomeServlet(private val serverLogging: ServerLogging) : HttpServlet(
         response.writer.println("Server IP Address: $localIpAddress")
     }
 
-    private fun getLocalIpAddress(): String {
+    private fun getLocalIpAddress(): String? {
         val networkInterfaces = NetworkInterface.getNetworkInterfaces()
         for (interfaceElement in Collections.list(networkInterfaces)) {
             val inetAddresses = interfaceElement.inetAddresses
             for (inetAddress in Collections.list(inetAddresses)) {
-                if (!inetAddress.isLoopbackAddress && inetAddress.hostAddress.indexOf(':') == -1) {
+                if (!inetAddress.isLoopbackAddress && inetAddress?.hostAddress?.indexOf(':') == -1) {
                     return inetAddress.hostAddress
                 }
             }
